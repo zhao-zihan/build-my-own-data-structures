39ad6d0c1902a1407134a4d0438a488cf7d2d255
Zihan Zhao <zihan0932@gmail.com>
2022-05-27 02:08:55 -0700

    Add variables and constructors

--- src/main/java/arraylist/MyArrayList.java
+++ src/main/java/arraylist/MyArrayList.java
@@ -1,4 +1,19 @@
 package arraylist;
 
-public class MyArrayList {
+public class MyArrayList<E> {
+    //Use an array as the base
+    private E[] data;
+    //Use an integer size to store the actual number of elements in the array
+    private int size;
+    //Set initial capacity to 1
+    private static final int INIT_CAP = 1;
+
+    public MyArrayList() {
+        this(INIT_CAP);
+    }
+
+    public MyArrayList(int initCapacity) {
+        data = (E[]) new Object[initCapacity];
+        size = 0;
+    }
 }
