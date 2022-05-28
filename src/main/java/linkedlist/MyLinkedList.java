33788e75a63787b594ad9ee2b97ccd88627d6b5b
Zihan Zhao <zihan0932@gmail.com>
2022-05-27 22:06:47 -0700

    Create MyLinkedList class and initialize variables and constructors

---
+++ src/main/java/linkedlist/MyLinkedList.java
@@ -0,0 +1,24 @@
+package linkedlist;
+
+public class MyLinkedList<E> {
+    private static class Node<E> {
+        E val;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(E val) {
+            this.val = val;
+        }
+    }
+
+    private final Node<E> head, tail;
+    private int size;
+
+    public MyLinkedList() {
+        this.head = new Node<>(null);
+        this.tail = new Node<>(null);
+        head.next = tail;
+        tail.prev = head;
+        this.size = 0;
+    }
+}
