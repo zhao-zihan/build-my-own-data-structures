a6af304eb7d54e0b7c8ad79c2d73e8f2c608998b
Zihan Zhao <zihan0932@gmail.com>
2022-06-07 21:44:03 -0700

    Complete all methods for add, delete, and read

--- src/main/java/queue/MyArrayDeque.java
+++ src/main/java/queue/MyArrayDeque.java
@@ -28,12 +28,25 @@ public class MyArrayDeque<E> {
         if (size == data.length) {
             resize(2 * data.length);
         }
+        if (first == 0) {
+            first = data.length - 1;
+        } else {
+            first--;
+        }
+        data[first] = e;
+        size++;
     }
 
     public void addLast(E e) {
         if (size == data.length) {
             resize(2 * data.length);
         }
+        data[last] = e;
+        last++;
+        if (last == data.length) {
+            last = 0;
+        }
+        size++;
     }
 
     /****** Delete ******/
@@ -42,12 +55,40 @@ public class MyArrayDeque<E> {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
+
+        if (size == data.length / 4) {
+            resize(data.length / 2);
+        }
+
+        E oldVal = data[first];
+        data[first] = null;
+        first++;
+        if (first == data.length) {
+            first = 0;
+        }
+        size--;
+        return oldVal;
     }
 
     public E removeLast() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
+
+        if (size == data.length / 4) {
+            resize(data.length / 2);
+        }
+
+        if (last == 0) {
+            last = data.length - 1;
+        } else{
+            last--;
+        }
+
+        E oldVal = data[last];
+        data[last] = null;
+        size--;
+        return oldVal;
     }
 
     /****** Read ******/
@@ -56,12 +97,17 @@ public class MyArrayDeque<E> {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
+        return data[first];
     }
 
     public E getLast() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
+        if (last == 0) {
+            return data[data.length - 1];
+        }
+        return data[last - 1];
     }
 
     /****** Tools ******/
